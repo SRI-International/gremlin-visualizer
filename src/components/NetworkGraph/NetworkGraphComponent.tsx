bd4414d0c06fd8fe0fd9de97173d7073efe760f7
Khang Duong <khang.duong@sri.com>
2023-05-10 11:31:47 -0700

    Adds zoom in/out and center controls.

--- src/components/NetworkGraph/NetworkGraphComponent.tsx
+++ src/components/NetworkGraph/NetworkGraphComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   setLayoutChanged,
@@ -8,10 +8,68 @@ import {
 } from '../../reducers/graphReducer';
 import { getNetwork } from '../../logics/network';
 import style from './NetworkGraphComponent.module.css';
+import { Network } from 'vis-network';
+import { Button, ButtonGroup, IconButton, Tooltip } from '@mui/material';
+import { Add, CenterFocusStrong, Remove } from '@mui/icons-material';
+
+type GraphControlsProps = {
+  network: Network;
+};
+
+type BasicProps = {
+  [key: string]: any;
+};
+
+const ButtonGroupIconButton = ({ ...props }: BasicProps) => {
+  // intercept props only implemented by `Button`
+  const { disableElevation, fullWidth, variant, ...iconButtonProps } = props;
+  return <IconButton {...iconButtonProps} />;
+};
+
+const GraphControls = ({ network }: GraphControlsProps) => {
+  const handleZoomIn = () => {
+    const scale = network.getScale();
+    network.moveTo({ scale: scale + 0.1 });
+  };
+
+  const handleZoomOut = () => {
+    const scale = network.getScale();
+    network.moveTo({ scale: scale - 0.1 });
+  };
+
+  const handleFitTo = () => {
+    network.fit();
+  };
+
+  return (
+    <ButtonGroup
+      variant="outlined"
+      orientation="vertical"
+      className={style['graph-controls']}
+    >
+      <Tooltip title="Zoom In">
+        <ButtonGroupIconButton onClick={handleZoomIn}>
+          <Add />
+        </ButtonGroupIconButton>
+      </Tooltip>
+      <Tooltip title="Fit Nodes to Screen">
+        <ButtonGroupIconButton onClick={handleFitTo}>
+          <CenterFocusStrong />
+        </ButtonGroupIconButton>
+      </Tooltip>
+      <Tooltip title="Zoom Out">
+        <ButtonGroupIconButton onClick={handleZoomOut}>
+          <Remove />
+        </ButtonGroupIconButton>
+      </Tooltip>
+    </ButtonGroup>
+  );
+};
 
 export const NetworkGraphComponent = () => {
   const dispatch = useDispatch();
   const { nodes, edges, selectedLayout } = useSelector(selectGraph);
+  const [network, setNetwork] = useState<Network | null>(null);
   const myRef = useRef(null);
 
   const selectNodeCallback = (params?: any) => {
@@ -35,20 +93,22 @@ export const NetworkGraphComponent = () => {
 
   useEffect(() => {
     if (myRef.current != null) {
-      const network = getNetwork(
-        myRef.current,
-        { nodes, edges },
-        selectedLayout,
-        {
+      setNetwork(
+        getNetwork(myRef.current, { nodes, edges }, selectedLayout, {
           selectNodeCallback,
           selectEdgeCallback,
           dragEndCallback,
-        }
+        })
       );
 
       // console.log(network?.getSeed());
     }
   }, [nodes, edges, selectedLayout]);
 
-  return <div ref={myRef} className={style['network-pane']} />;
+  return (
+    <div className={style['graph-container']}>
+      <div ref={myRef} className={style['graph-pane']} />
+      {network && <GraphControls network={network} />}
+    </div>
+  );
 };
