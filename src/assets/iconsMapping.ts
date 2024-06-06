ecd800ae602563cf60970deb21f6424259d0009f
joon-at-sri <joon.choi@sri.com>
2024-06-06 09:45:14 -0700

    oops

--- src/assets/iconsMapping.ts
+++ src/assets/iconsMapping.ts
@@ -1,8 +1,19 @@
 
+import icons from './icons/icons.json';
 import man_37x37_icon from './icons/man_37x37_icon.png';
 
-const icons: Record<string, string> = {
+const icons = {
+  'icons': icons,
   'man_37x37_icon': man_37x37_icon,
 };
 
 export default icons;
+
+
+// import man_37x37_icon from './icons/man_37x37_icon.png';
+
+// const icons: Record<string, string> = {
+//   'man_37x37_icon': man_37x37_icon,
+// };
+
+// export default icons;
