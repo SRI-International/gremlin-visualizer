2408d8f4afdccaad9186b3a70f75dca115faa0fb
joon-at-sri <joon.choi@sri.com>
2024-06-27 11:17:52 -0700

    page, dense, sort

--- src/components/Details/TableComponent.tsx
+++ src/components/Details/TableComponent.tsx
@@ -15,60 +15,66 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { selectGraph } from '../../reducers/graphReducer';
 import { useSelector } from 'react-redux';
 import { EdgeData, NodeData } from '../../logics/utils';
-import { IdType } from 'vis-network';
+import { Data, IdType } from 'vis-network';
 import { Key } from '@mui/icons-material';
+import { Checkbox, FormControlLabel, Switch, TablePagination, TableSortLabel } from '@mui/material';
+import { visuallyHidden } from '@mui/utils';
 
 
 type RowData = {
     id: IdType,
-    elementType : string,
+    elementType: string,
     type: string,
-    displayLabel : string,
+    displayLabel: string,
     additionalAttributes: { [key: string]: any };
 };
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof RowData;
+    label: string;
+    numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: 'id',
+        numeric: true,
+        disablePadding: true,
+        label: 'ID',
+    },
+    {
+        id: 'elementType',
+        numeric: false,
+        disablePadding: false,
+        label: 'Element Type',
+    },
+    {
+        id: 'type',
+        numeric: false,
+        disablePadding: false,
+        label: 'Type',
+    },
+    {
+        id: 'displayLabel',
+        numeric: false,
+        disablePadding: false,
+        label: 'Label',
+    },
+];
 
 function createData(nodes: Array<NodeData>, edges: Array<EdgeData>) {
     let rows: RowData[] = [];
     nodes.forEach((node) => {
-        let data : RowData = {id: node.id, elementType : 'node', type: node.type, displayLabel: node.label, additionalAttributes: node.properties};
+        let data: RowData = { id: node.id, elementType: 'node', type: node.type, displayLabel: node.label, additionalAttributes: node.properties };
         rows.push(data);
     })
     edges.forEach((edge) => {
-        let data : RowData = {id: edge.id, elementType: 'edge', type: edge.type, displayLabel: edge.label, additionalAttributes: {...edge.properties, from: edge.from, to:edge.to}};
+        let data: RowData = { id: edge.id, elementType: 'edge', type: edge.type, displayLabel: edge.label, additionalAttributes: { ...edge.properties, from: edge.from, to: edge.to } };
         rows.push(data);
     })
     return rows;
 }
 
-// function createData(
-//     name: string,
-//     calories: number,
-//     fat: number,
-//     carbs: number,
-//     protein: number,
-//     price: number,
-// ) {
-//     return {
-//         name,
-//         calories,
-//         fat,
-//         carbs,
-//         protein,
-//         price,
-//         history: [
-//             {
-//                 date: '2020-01-05',
-//                 customerId: '11091700',
-//                 amount: 3,
-//             },
-//             {
-//                 date: '2020-01-02',
-//                 customerId: 'Anonymous',
-//                 amount: 1,
-//             },
-//         ],
-//     };
-// }
 
 
 function Row(props: { row: RowData }) {
@@ -77,7 +83,7 @@ function Row(props: { row: RowData }) {
 
     return (
         <React.Fragment>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' }, '&:last-child td, &:last-child th': { border: 0 }}}>
                 <TableCell>
                     <IconButton
                         aria-label="expand row"
@@ -103,14 +109,14 @@ function Row(props: { row: RowData }) {
                             </Typography>
                             <Table size="small" aria-label="additional-info">
                                 <TableHead>
-                                    <TableRow>
+                                    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                         <TableCell>Name</TableCell>
                                         <TableCell>Value</TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
                                     {Object.entries(row.additionalAttributes).map(([name, value]) => (
-                                        <TableRow key={name}>
+                                        <TableRow key={name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                             <TableCell component="th" scope="row">
                                                 {name}
                                             </TableCell>
@@ -127,31 +133,168 @@ function Row(props: { row: RowData }) {
     );
 }
 
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    const aValue = a[orderBy];
+    const bValue = b[orderBy];
+
+    if (typeof aValue === 'string' && typeof bValue === 'string') {
+        const numA = Number(aValue);
+        const numB = Number(bValue);
+        if (!isNaN(numA) && !isNaN(numB)) {
+            if (numB < numA) {
+                return -1;
+            }
+            if (numB > numA) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    if (typeof aValue === 'string' && typeof bValue === 'string') {
+        return aValue.localeCompare(bValue);
+    }
+    if (typeof aValue === 'number' && typeof bValue === 'number') {
+        if (bValue < aValue) {
+            return -1;
+        }
+        if (bValue > aValue) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+type SortableKeys = Exclude<keyof RowData, 'additionalAttributes'>;
+function getComparator<Key extends SortableKeys>(
+    order: Order,
+    orderBy: Key,
+): (
+    a: { [key in Key]: any },
+    b: { [key in Key]: any },
+) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface EnhancedTableProps {
+    onRequestSort: (event: React.MouseEvent<unknown>, property: SortableKeys) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const { order, orderBy, rowCount, onRequestSort } =
+        props;
+    const createSortHandler =
+        (property: SortableKeys) => (event: React.MouseEvent<unknown>) => {
+            onRequestSort(event, property as SortableKeys);
+        };
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell />
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align="right"
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id as SortableKeys)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
 
 export default function CollapsibleTable() {
     const { nodes, edges } = useSelector(selectGraph);
     const rows = createData(nodes as NodeData[], edges as EdgeData[]);
-    console.log(nodes);
-    console.log(edges); 
-    console.log("yes");
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<SortableKeys>('id');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (
+        event: React.MouseEvent<unknown>,
+        property: SortableKeys,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const visibleRows = React.useMemo(
+        () =>
+            rows.slice().sort(getComparator(order, orderBy)).slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage,
+            ),
+        [order, orderBy, page, rowsPerPage],
+    );
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="collapsible table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell />
-                        <TableCell>id</TableCell>
-                        <TableCell align="right">elementType</TableCell>
-                        <TableCell align="right">Type</TableCell>
-                        <TableCell align="right">Label</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <Row key={row.id} row={row} />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Box sx={{ width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="collapsible table" size="small">
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={rows.length}
+                        />
+                        <TableBody>
+                            {visibleRows.map((row) => (
+                                <Row key={row.id} row={row} />
+                            ))}
+                            {emptyRows > 0 && (
+                                <TableRow>
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
     );
 }
