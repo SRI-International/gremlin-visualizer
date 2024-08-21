90e938e558f73ed885d76ffdeeaa0e3c9e03d40a
Brian Blonski <brian.blonski@sri.com>
2024-08-20 17:49:04 -0700

    icon type mapping

--- src/constants.js
+++ src/constants.js
@@ -31,6 +31,16 @@ export const SAVED_QUERIES = {
     // "get person nodes that marko has outgoing edges to" : "g.V().has('name', 'marko').out().hasLabel('person')"
 }
 
+const software = require('./assets/icons/binary_icon_tiny_square_v1.png');
+const operatingSystem = require('./assets/icons/operating_system_icon_tiny_round_v1.png');
+const firmware = require('./assets/icons/firmware_icon_v1.png');
+const wifiDriver = require('./assets/icons/wifi_icon_tiny_round_v1.png');
+const library = require('./assets/icons/library_icon_tiny_square_v1.png');
+const libraries = require('./assets/icons/libraries_icon_tiny_square_v1.png');
+const personalityDarkTriad = require('./assets/icons/psychology/personality_dark_triad.png');
+const traitEmotionNegative = require('./assets/icons/psychology/trait_emotion_negative.png');
+const traitEmotionPositive = require('./assets/icons/psychology/trait_emotion_positive.png');
+const traitEmotionNeutral = require('./assets/icons/psychology/trait_emotion_neutral.png');
 /**
  * Icon string to image mapping.
  * Nodes with label matching the map key will load the associated icon.
@@ -39,7 +49,7 @@ export const ICONS = {
     artifact: require('./assets/icons/artifact_icon_tiny_round_v1.png'),
     autonomous_system: require('./assets/icons/autonomous_system_icon_tiny_round_v1.png'),
     autonomous_system_product: require('./assets/icons/autonomous_system_product_icon_tiny_round_v1.png'),
-    binary: require('./assets/icons/binary_icon_tiny_square_v1.png'),
+    binary: software,
     bluetooth: require('./assets/icons/bluetooth_icon_tiny_round_v1.png'),
     bug: require('./assets/icons/bug_icon_tiny_square_v1.png'),
     campaign: require('./assets/icons/campaign_icon_tiny_round_v1.png'),
@@ -53,7 +63,7 @@ export const ICONS = {
     email_addr: require('./assets/icons/email_addr_icon_tiny_round_v1.png'),
     email_message: require('./assets/icons/email_message_icon_tiny_round_v1.png'),
     file: require('./assets/icons/file_icon_tiny_round_v1.png'),
-    firmware: require('./assets/icons/firmware_icon_v1.png'),
+    firmware: firmware,
     // flowchart: require('./assets/icons/flowchart_icon_tiny_square_v1.png'),
     hardware: require('./assets/icons/hardware_icon_v1.png'),
     http: require('./assets/icons/http_icon_tiny_round_v1.png'),
@@ -63,8 +73,8 @@ export const ICONS = {
     infrastructure: require('./assets/icons/infrastructure_icon_tiny_round_v1.png'),
     intrusion_set: require('./assets/icons/intrusion_set_icon_tiny_round_v1.png'),
     language: require('./assets/icons/language_icon_tiny_round_v1.png'),
-    library: require('./assets/icons/library_icon_tiny_square_v1.png'),
-    libraries: require('./assets/icons/libraries_icon_tiny_square_v1.png'),
+    library: library,
+    libraries: libraries,
     location: require('./assets/icons/location_icon_tiny_round_v1.png'),
     malware_analysis: require('./assets/icons/malware_analysis_icon_tiny_round_v1.png'),
     malware: require('./assets/icons/malware_icon_tiny_round_v1.png'),
@@ -77,7 +87,7 @@ export const ICONS = {
     note: require('./assets/icons/note_icon_tiny_round_v1.png'),
     observed_data: require('./assets/icons/observed_data_icon_tiny_round_v1.png'),
     opinion: require('./assets/icons/opinion_icon_tiny_round_v1.png'),
-    operating_system: require('./assets/icons/operating_system_icon_tiny_round_v1.png'),
+    operating_system: operatingSystem,
     process: require('./assets/icons/process_icon_tiny_round_v1.png'),
     printer: require('./assets/icons/printer_icon_tiny_square_v1.png'),
     relationship: require('./assets/icons/relationship_icon_tiny_round_v1.png'),
@@ -96,7 +106,7 @@ export const ICONS = {
     vulnerability: require('./assets/icons/vulnerability_icon_tiny_square_v1.png'),
     vulnerability_template: require('./assets/icons/vulnerability_template_icon_tiny_square_v1.png'),
     web_browser: require('./assets/icons/web_browser_icon_tiny_round_v1.png'),
-    wifi: require('./assets/icons/wifi_icon_tiny_round_v1.png'),
+    wifi: wifiDriver,
     workstation: require('./assets/icons/workstation_icon_tiny_square_v1.png'),
     windows_registry_key: require('./assets/icons/windows_registry_key_icon_tiny_round_v1.png'),
     x509_certificate: require('./assets/icons/x509_certificate_icon_tiny_square_v1.png'),
@@ -121,25 +131,25 @@ export const ICONS = {
     cognitive_attention: require('./assets/icons/psychology/attention.png'),
     cognitive_fatigue: require('./assets/icons/psychology/fatigue.png'),
     cognitive_workload: require('./assets/icons/psychology/workload.png'),
-    trait_emotion_negative: require('./assets/icons/psychology/trait_emotion_negative.png'),
-    trait_emotion_positive: require('./assets/icons/psychology/trait_emotion_positive.png'),
-    trait_emotion_neutral: require('./assets/icons/psychology/trait_emotion_neutral.png'),
+    trait_emotion_negative: traitEmotionNegative,
+    trait_emotion_positive: traitEmotionPositive,
+    trait_emotion_neutral: traitEmotionNeutral,
     emotional_states: require('./assets/icons/psychology/emotional_states.png'),
     personality_normal: require('./assets/icons/psychology/personality_normal.png'),
-    personality_dark_triad: require('./assets/icons/psychology/personality_dark_triad.png'),
+    personality_dark_triad: personalityDarkTriad,
 
     // Cultures
-    american: require('./assets/icons/culture/us.png'),
-    chinese: require('./assets/icons/culture/cn.png'),
-    french: require('./assets/icons/culture/fr.png'),
-    indian: require('./assets/icons/culture/in.png'),
-    iranian: require('./assets/icons/culture/ir.png'),
-    iraqi: require('./assets/icons/culture/iq.png'),
-    israeli: require('./assets/icons/culture/is.png'),
-    north_korean: require('./assets/icons/culture/kp.png'),
-    pakistani: require('./assets/icons/culture/pk.png'),
-    russian: require('./assets/icons/culture/ru.png'),
-    unknown: require('./assets/icons/culture/unknown.png'),
+    american_culture: require('./assets/icons/culture/us.png'),
+    chinese_culture: require('./assets/icons/culture/cn.png'),
+    french_culture: require('./assets/icons/culture/fr.png'),
+    indian_culture: require('./assets/icons/culture/in.png'),
+    iranian_culture: require('./assets/icons/culture/ir.png'),
+    iraqi_culture: require('./assets/icons/culture/iq.png'),
+    israeli_culture: require('./assets/icons/culture/is.png'),
+    north_korean_culture: require('./assets/icons/culture/kp.png'),
+    pakistani_culture: require('./assets/icons/culture/pk.png'),
+    russian_culture: require('./assets/icons/culture/ru.png'),
+    culture_base: require('./assets/icons/culture/unknown.png'),
 
     // Logic
     logical_and: require('./assets/icons/logic/logical_and_icon_v1.png'),
@@ -149,4 +159,99 @@ export const ICONS = {
     logical_nxor: require('./assets/icons/logic/logical_nxor_icon_v1.png'),
     logical_or: require('./assets/icons/logic/logical_or_icon_v1.png'),
     logical_xor: require('./assets/icons/logic/logical_xor_icon_v1.png'),
+
+
+    os_runtime: software,
+    application_framework: software,
+    android_runtime: software,
+    android_framework: software,
+
+    operating_system_kernel: operatingSystem,
+    android_kernel: operatingSystem,
+
+    boot_loader: firmware,
+    android_boot_loader: firmware,
+
+    hardware_driver: software,
+    hardware_abstraction_layer: software,
+    hardware_abstraction: software,
+    android_kernel_binder: software,
+    android_hardware_abstraction_layer: software,
+    android_storage_driver: software,
+    android_binder_ha: software,
+
+    android_memory_driver: require('./assets/icons/memory_icon_tiny_round_v1.png'),
+    android_shared_memory_library: require('./assets/icons/memory_icon_tiny_round_v1.png'),
+
+    telephony_ha: require('./assets/icons/cellular_icon_tiny_round_v1.png'),
+    android_telephony_ha: require('./assets/icons/cellular_icon_tiny_round_v1.png'),
+
+    wifi_driver: wifiDriver,
+    wifi_ha: wifiDriver,
+    android_wifi_driver: wifiDriver,
+    android_wifi_ha: wifiDriver,
+
+    android_bluetooth_library: require('./assets/icons/bluetooth_icon_tiny_round_v1.png'),
+
+    camera_library: require('./assets/icons/camera_icon_tiny_round_v1.png'),
+    android_camera_library: require('./assets/icons/camera_icon_tiny_round_v1.png'),
+
+    media_ha: libraries,
+    media_library: libraries,
+    android_media_ha: libraries,
+    android_media_library: libraries,
+
+    android_native_libraries: libraries,
+    android_core_library: libraries,
+
+    android_ssl_library: require('./assets/icons/x509_certificate_icon_tiny_square_v1.png'),
+
+    android_debuggerd_library: library,
+    android_libutils: library,
+    android_binder_library: library,
+    android_open_gl_library: library,
+    android_libc: library,
+
+    pov: require('./assets/icons/malware_icon_tiny_round_v1.png'),
+
+    attention: require('./assets/icons/psychology/attention.png'),
+    fatigue: require('./assets/icons/psychology/fatigue.png'),
+    workload: require('./assets/icons/psychology/workload.png'),
+
+    anger: traitEmotionNegative,
+    anxiety: traitEmotionNegative,
+    frustration: traitEmotionNegative,
+    stress: traitEmotionNegative,
+
+    risk_aversion: traitEmotionNeutral,
+    confidence: traitEmotionPositive,
+
+    personality_narcissism: personalityDarkTriad,
+    personality_machiavellianism: personalityDarkTriad,
+    personality_psychopathy: personalityDarkTriad,
+
+    agreeableness: traitEmotionPositive,
+    conscientiousness: traitEmotionPositive,
+    openness: traitEmotionPositive,
+    stability: traitEmotionPositive,
+
+    extraversion: traitEmotionNeutral,
+
+    dominance: traitEmotionNegative,
+    entitlement: traitEmotionNegative,
+    grandiosity: traitEmotionNegative,
+    superiority: traitEmotionNegative,
+
+    callousness: traitEmotionNegative,
+    cynicism: traitEmotionNegative,
+    manipulativeness: traitEmotionNegative,
+    strategic_calculating: traitEmotionNegative,
+
+    antisocial: traitEmotionNegative,
+    impulsivity: traitEmotionNegative,
+    thrill_seeking: traitEmotionNegative,
+
+    empathy: traitEmotionPositive,
+    self_control: traitEmotionPositive,
+
 };
